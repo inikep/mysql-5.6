e34cb7269f4d2eba51c45e33ae4429045cf77241
Santosh Praneeth Banda <santoshb@fb.com>
2015-02-10 00:36:33 -0800

    New conf option for TCP input buffer size in replication readers
    Summary:
    A new conf option rpl_receive_buffer_size is added
    to control the input buffer size of the socket used in slave io_thread.
    
    A new cmd line option --receive_buffer_size is added to mysqlbinlog utility
    to control the input buffer size of socket used for connection to the server.
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/87ca25c
    
    Differential Revision: D7080130

--- client/mysqlbinlog.cc
+++ client/mysqlbinlog.cc
@@ -818,6 +818,8 @@ static bool in_transaction = false;
 /* It is set to true when GTID is found, and false when the transaction ends. */
 static bool seen_gtid = false;
 
+static uint opt_receive_buffer_size = 0;
+
 static Exit_status dump_local_log_entries(PRINT_EVENT_INFO *print_event_info,
                                           const char *logname);
 static Exit_status dump_remote_log_entries(PRINT_EVENT_INFO *print_event_info,
@@ -2087,6 +2089,13 @@ static struct my_option my_long_options[] = {
      "of temporary tables.",
      &opt_require_row_format, &opt_require_row_format, nullptr, GET_BOOL,
      NO_ARG, 0, 0, 0, nullptr, 0, nullptr},
+    {"receive-buffer-size", OPT_RECEIVE_BUFFER_SIZE,
+     "The size of input buffer for the socket used while "
+     "receving events from the server",
+     &opt_receive_buffer_size, &opt_receive_buffer_size, nullptr, GET_UINT,
+     REQUIRED_ARG,
+     /* def_val */ 1024 * 1024, /* min_value */ 1024, /* max_value */ UINT_MAX,
+     nullptr, /* block_size */ 1024, nullptr},
     {nullptr, 0, nullptr, nullptr, nullptr, nullptr, GET_NO_ARG, NO_ARG, 0, 0,
      0, nullptr, 0, nullptr},
 };
@@ -2384,6 +2393,9 @@ static Exit_status safe_connect() {
                  "binary_log_listener");
   set_server_public_key(mysql_handle);
   set_get_server_public_key_option(mysql_handle);
+
+  mysql_options(mysql_handle, MYSQL_OPT_NET_RECEIVE_BUFFER_SIZE,
+                &opt_receive_buffer_size);
 
   if (!mysql_real_connect(mysql_handle, host, user, pass, nullptr, port, sock,
                           0)) {
