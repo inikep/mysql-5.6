66d77157113bff87de66047c88be8ca0d876a787
Zhicheng Zhu <zhichengzhu@fb.com>
2019-06-24 14:28:48 -0700

    [dd_version] Add table for queries_used and queries_empty
    Summary:
    Adding queries_used and empty_queries instruments for performance schema.
    Since this diff is more than 500 lines. Let me talk about the code structure. This diff contains three parts
    1. Adding instrument data(query_stat)
    2. Create performance_schema.table_statistics_per_table for querying query_stat
    3. Adding tests and update existing tests
    
    There are three things I thought about when writing this code.
    * Where should I add the query_stat and how to do the aggregation for query_stats? After reading other pfs code. Since query_stat is a part of table_stat, I added it to pfs_stat.h
    
    * Since we create a new stat which is related to table. There are two places -- PFS_table and PFS_table_share we need to considered.  For details, look at pfs_instr.cc/pfs_instr.h and pfs_instr_class.cc/pfs_instr_class.h
       *  PFS_table.  -- This is per table instance, which means there can be multiple PFS_table point to the same sql table
       *  PFS_table_share -- All the table instance will have a pointer points to this object. If a sql table is flushed or closed, we will dump all the PFS_table query_stat data to the PFS_table_share query_stat.
    * Where can we collect the data?  Based on our 5.6 implementation. If there is an io event, then that mean we have a query and we should bump the queries_used counter. If the sql command is select and sent_rows is 0, then we should bump the empty_queries counter. In this case we need to modify pfs_end_table_io_wait_v1 function to know if there is a query, then bump counters at pfs_unbind_table_v1. Originally I was thinking put all those logic into pfs_end_table_io_wait_v1, but I realized that a single query can have multiple io operations. This we should only bump query_stat counter in pfs_unbind_table_v1. For details, look at pfs.cc
    
    I basically use the storage/perfschema/table_tlws_by_table.cc as the template.
    
    Differential Revision: D15974071

--- storage/perfschema/pfs_dd_version.h
+++ storage/perfschema/pfs_dd_version.h
@@ -259,10 +259,14 @@
   table_users
   table_variables_info
 
+  80300-003:
+  performance_schema tables changed:
+  - table_statistics_per_table added.
+
   The last three digits reprents Facebook specific MySQL Schema changes.
-  Version published is now 80300-002. i.e. 8.3.0 Facebook schema change no. 2.
+  Version published is now 80300-003. i.e. 8.3.0 Facebook schema change no. 3.
 */
 
-static const uint PFS_DD_VERSION = 80300002;
+static const uint PFS_DD_VERSION = 80300003;
 
 #endif /* PFS_DD_VERSION_H */
--- storage/perfschema/pfs_events_waits.cc
+++ storage/perfschema/pfs_events_waits.cc
@@ -249,6 +249,14 @@ void reset_table_io_waits_by_table() {
   global_table_share_container.apply(fct_reset_table_io_waits_by_table);
 }
 
+static void fct_reset_table_statistics_by_table(PFS_table_share *pfs) {
+  pfs->reset_query_stat();
+}
+
+void reset_table_statistics_by_table() {
+  global_table_share_container.apply(fct_reset_table_statistics_by_table);
+}
+
 static void fct_reset_table_lock_waits_by_table(PFS_table_share *pfs) {
   pfs->aggregate_lock();
 }
@@ -279,4 +287,12 @@ static void fct_reset_table_lock_waits_by_table_handle(PFS_table *pfs) {
 
 void reset_table_lock_waits_by_table_handle() {
   global_table_container.apply(fct_reset_table_lock_waits_by_table_handle);
+}
+
+static void fct_reset_table_statistics_by_table_handle(PFS_table *pfs) {
+  pfs->sanitized_aggregate_query();
+}
+
+void reset_table_statistics_by_table_handle() {
+  global_table_container.apply(fct_reset_table_statistics_by_table_handle);
 }
