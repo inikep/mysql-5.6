08a2d49197e63e637481414e98200c7f7a524d5a
Zhicheng Zhu <zhichengzhu@fb.com>
2020-01-10 17:30:06 -0800

    add skip master info for read_only_msg_extra (#1080) (#1080)
    Summary:
    Since it may take some time for smc for propagate new master info,
    people use read_only_msg_extra to get new master info from old master.
    However we still have a check for active master info in mysqld. And
    update active master info may take a few seconds. This might cause a
    downtime even a new master is kind of ready. Current promote_master
    command can specifically set new master info during the promotion. It's
    actually fine to skip the active master info check. For compatibility,
    let's add a new sys var to pass the active master info check now
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/80239b6c7b0
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1080
    
    Reviewed By: zhichengzhu
    
    Differential Revision: D19357081
    
    Pulled By: zhichengzhu

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -7566,6 +7566,12 @@ static Sys_var_bool Sys_replication_sender_observe_commit_only(
 Debug_shutdown_actions Debug_shutdown_actions::instance;
 #endif
 
+static Sys_var_bool Sys_skip_master_info_check_for_read_only_error_msg_extra(
+    "skip_master_info_check_for_read_only_error_msg_extra",
+    "Skip master info validaton check for read only error messages",
+    GLOBAL_VAR(skip_master_info_check_for_read_only_error_msg_extra),
+    CMD_LINE(OPT_ARG), DEFAULT(false));
+
 #ifdef HAVE_JEMALLOC
 ulong enable_jemalloc_hpp;
 
