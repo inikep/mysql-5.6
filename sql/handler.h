cbf53419458759e0f5b4b6a446c6557fb8c0ab9b
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-04-08 15:27:47 +0200

    FB8-181: Maintain a whitelist of semi-sync slaves which can ACK transactions (#1017) (#1017)
    Summary:
    Jira issue: https://jira.percona.com/browse/FB8-181
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/ee761fc
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/68f34521e82
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/dfb038d48d0
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/de1e8c7bfe7
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/26835a39459
    
    A whitelist of semi-sync slaves can be maintained using a new sys-var
    `rpl_semi_sync_master_whitelist`. The value of this variable is a comma
    separated list of UUIDs. ACK from any slave which is not in the whitelist will
    be ignored. The whitelist is persisted in file
    `semi_sync_master_whitelist.info` to survive server restarts.
    
    Porting notice:
    You have to use `set persist.rpl_semi_sync_master_whitelist` to keep
    value across server restarts.
    
    Originally Reviewed By: Tema
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1017
    
    Differential Revision: D15285223
    
    Pulled By: yizhang82
    
    --------------------------------------------------------------------------
    
    Fix rpl.rpl_semi_sync_master_whitelist test in valgrind
    
    Summary:
    Increase the retry counts for valgrind runs.
    
    Also increase the shutdown_timeout for valgrind runs to give more time
    for shutdown to complete.
    
    Reviewed By: Pushapgl
    
    Differential Revision: D33359920
    
    fbshipit-source-id: 68210acc3a6

--- sql/handler.h
+++ sql/handler.h
@@ -7110,4 +7110,7 @@ class ha_tablespace_statistics {
   dd::String_type m_extra;  // NDB only
 };
 
+std::unordered_set<std::string> split_into_set(const std::string &input,
+                                               char delimiter);
+
 #endif /* HANDLER_INCLUDED */
