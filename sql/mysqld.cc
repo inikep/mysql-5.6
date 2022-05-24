voodam/percona-server

Add zstd compression option for replication
Summary:
This diff adds a generic protocol option to modify the compression
algorithm at connect time. This is done using the 'compression_lib' connection
attribute, which when combined with the compress flag, will determine a new
compression library for network compression. It is meant to be extendable to
other compression libraries.

Protocol info:
When a client wants to use compression, they can either set the compress flag
as they do now, or use mysql_options to select a compression library from the
given enums. Under the hood, this will set the compress flag and pass the
library through to the server via a connection attribute

Rollout:
To change the compression on slave threads connect to the slave and run
'SET slave_compression_lib="zstd";stop slave;start slave;'
This assumes the master also supports the same compression algorithms.
In the event the master is not upgraded an error will occur on the slave side

Reference patch: https://github.com/facebook/mysql-5.6/commit/a16bf0c

Reviewed By: abal147

Differential Revision: D7142958

fbshipit-source-id: bdff8c7323d

sql/mysqld.cc
@@ -1037,6 +1037,7 @@ bool opt_skip_slave_start = false;  ///< If set, slave is not autostarted
 bool opt_enable_named_pipe = false;
 bool opt_local_infile, opt_slave_compressed_protocol;
 bool opt_safe_user_create = false;
+ulong opt_slave_compression_lib = 0;
 bool opt_show_slave_auth_info;
 bool opt_log_slave_updates = false;
 char *opt_slave_skip_errors;
@@ -1075,6 +1076,7 @@ bool opt_myisam_use_mmap = false;
 std::atomic<bool> offline_mode;
 uint opt_large_page_size = 0;
 uint net_compression_level = 6;
+uint zstd_net_compression_level = 3;
 uint default_password_lifetime = 0;
 bool password_require_current = false;
 std::atomic<bool> partial_revokes;

sql/mysqld.h
@@ -174,6 +174,7 @@ extern bool opt_initialize;
 extern bool opt_safe_user_create;
 extern bool opt_local_infile, opt_myisam_use_mmap;
 extern bool opt_slave_compressed_protocol;
+extern ulong opt_slave_compression_lib;
 extern ulong slave_exec_mode_options;
 extern Rpl_global_filter rpl_global_filter;
 extern int32_t opt_regexp_time_limit;
@@ -347,6 +348,7 @@ extern ulonglong relay_io_bytes, relay_sql_bytes;
 extern ulonglong relay_sql_wait_time;
 
 extern uint net_compression_level;
+extern uint zstd_net_compression_level;
 
 /* SHOW STATS var: Name of current timer */
 extern const char *timer_in_use;

sql/sys_vars.cc
@@ -4316,6 +4316,20 @@ static Sys_var_uint Sys_net_compression_level(
     GLOBAL_VAR(net_compression_level), CMD_LINE(REQUIRED_ARG),
     VALID_RANGE(0, 9), DEFAULT(6), BLOCK_SIZE(1));
 
+const char *mysql_compression_lib_names[] = {"zlib", "zstd", nullptr};
+static Sys_var_enum slave_compression_lib_enum(
+    "slave_compression_lib",
+    "Compression library for replication stream. 0 is zlib, 1 is zstd.",
+    GLOBAL_VAR(opt_slave_compression_lib), CMD_LINE(OPT_ARG),
+    mysql_compression_lib_names, DEFAULT(0), NO_MUTEX_GUARD, NOT_IN_BINLOG);
+
+static Sys_var_uint Sys_zstd_net_compression_level(
+    "zstd_net_compression_level",
+    "Compression level for compressed protocol when zstd library is"
+    " selected. Valid values 0-22.",
+    GLOBAL_VAR(zstd_net_compression_level), CMD_LINE(OPT_ARG),
+    VALID_RANGE(0, 22), DEFAULT(3), BLOCK_SIZE(1));
+
 static Sys_var_ulong Sys_sort_buffer(
     "sort_buffer_size",
     "Each thread that needs to do a sort allocates a buffer of this size",

inikep/mysql-5.6

sql/mysqld.cc
@@ -1208,6 +1208,7 @@ bool opt_skip_replica_start = false;  ///< If set, slave is not autostarted
 bool opt_enable_named_pipe = false;
 bool opt_local_infile, opt_replica_compressed_protocol;
 bool opt_safe_user_create = false;
+ulong opt_replica_compression_lib = 0;
 bool opt_show_replica_auth_info;
 bool opt_log_replica_updates = false;
 char *opt_replica_skip_errors;
@@ -1246,6 +1247,7 @@ bool opt_myisam_use_mmap = false;
 std::atomic<bool> offline_mode;
 uint opt_large_page_size = 0;
 uint net_compression_level = 6;
+uint zstd_net_compression_level = 3;
 uint default_password_lifetime = 0;
 bool password_require_current = false;
 std::atomic<bool> partial_revokes;

sql/mysqld.h
@@ -191,6 +191,7 @@ extern bool opt_initialize;
 extern bool opt_safe_user_create;
 extern bool opt_local_infile, opt_myisam_use_mmap;
 extern bool opt_replica_compressed_protocol;
+extern ulong opt_replica_compression_lib;
 extern ulong replica_exec_mode_options;
 extern Rpl_global_filter rpl_global_filter;
 extern Rpl_acf_configuration_handler *rpl_acf_configuration_handler;
@@ -371,6 +372,7 @@ extern ulonglong relay_io_bytes, relay_sql_bytes;
 extern ulonglong relay_sql_wait_time;
 
 extern uint net_compression_level;
+extern uint zstd_net_compression_level;
 
 /* SHOW STATS var: Name of current timer */
 extern const char *timer_in_use;

sql/sys_vars.cc
@@ -4734,6 +4734,20 @@ static Sys_var_uint Sys_net_compression_level(
     GLOBAL_VAR(net_compression_level), CMD_LINE(REQUIRED_ARG),
     VALID_RANGE(0, 9), DEFAULT(6), BLOCK_SIZE(1));
 
+const char *mysql_compression_lib_names[] = {"zlib", "zstd", nullptr};
+static Sys_var_enum slave_compression_lib_enum(
+    "slave_compression_lib",
+    "Compression library for replication stream. 0 is zlib, 1 is zstd.",
+    GLOBAL_VAR(opt_replica_compression_lib), CMD_LINE(OPT_ARG),
+    mysql_compression_lib_names, DEFAULT(0), NO_MUTEX_GUARD, NOT_IN_BINLOG);
+
+static Sys_var_uint Sys_zstd_net_compression_level(
+    "zstd_net_compression_level",
+    "Compression level for compressed protocol when zstd library is"
+    " selected. Valid values 0-22.",
+    GLOBAL_VAR(zstd_net_compression_level), CMD_LINE(OPT_ARG),
+    VALID_RANGE(0, 22), DEFAULT(3), BLOCK_SIZE(1));
+
 static Sys_var_ulong Sys_sort_buffer(
     "sort_buffer_size",
     "Each thread that needs to do a sort allocates a buffer of this size",