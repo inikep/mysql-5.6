29740da9c8a8e64570faa5e60cc2cb4de40797bb
Aditya Jalan <aditj@meta.com>
2022-12-29 15:57:16 -0800

    [privacy] Expose api to fetch table privacy option in internal api
    Summary: Expose api to fetch table privacy option in internal api
    
    Reviewed By: lth, george-reynya
    
    Differential Revision: D42285033

--- sql/sql_thd_internal_api.h
+++ sql/sql_thd_internal_api.h
@@ -391,4 +391,20 @@ const std::string &thd_get_connection_attr(THD *thd,
 void thd_add_response_attr(THD *thd, const std::string &rattr_key,
                            const std::string &rattr_val);
 
+/**
+  Get the privacy_policy option value for the table
+  @param  thd             Thread handler
+  @param  db_name         Database name
+  @param  table_name      Table name
+  @param  privacy_policy_str Privacy Policy Str.This string is allocated in
+                             MemRoot and will be freed when query finishes.
+
+  @return true  - Return value follows success semantics.
+                  On success, if able to get the privacy policy
+  @return false - otherwise
+*/
+bool thd_get_table_privacy_policy(THD *thd, const std::string &db_name,
+                                  const std::string &table_name,
+                                  LEX_STRING *privacy_policy_str);
+
 #endif  // SQL_THD_INTERNAL_API_INCLUDED
