inikep/mysql-5.6

commit d630c52b336afe5150e5bf7fb510f8c2053d1aa0

Convert LOCK_thd_security_ctx to MDL mutex
Summary:
`LOCK_thd_security_ctx` mutex is held across `show processlist` and queries from `information_schema.processlist`. It is also held when `COM_CHANGE_USER` is executed. This is prone to yielding with held lock, and in case of `COM_CHANGE_USER` it performs the switch between databases. The switch results in moving the connection into the system AC info. As a part of the move the worker enters the TP scheduler, and the held lock is detected. Collecting the stack trace for the held lock takes a long time (up to 1 second) and triggers the stuck scheduler detection. Avoiding this detection will be done in another diff. Nevertheless, the detection moves the worker off the scheduler, and when it enters the system AC info it has to get enqueued in the runnable queue instead of just being admitted directly. This resulted in the assert that failed the `main.plugin` test.

This fix is to replace the mutex with MDL lock which is TP friendly and doesn't trigger the held lock detection. Since this mutex is per `THD` object, the corresponding MDL lock would have to have a unique name that could be constructed by using the `THD` object address. This will also result in many MDL locks inserted in the MDL hash table. While I was able to confirm that the growth of MDL lock objects is bounded as the lock objects are reused, still I observed about 1K objects created resulting in ~500KB of extra memory usage.

Memory usage after running this test script: P700001880.
```
mysql> select * from performance_schema.memory_summary_global_by_event_name where event_name like '%lf_node%';
+----------------------+-------------+------------+---------------------------+--------------------------+----------------+--------------------+-----------------+--------------------------+------------------------------+---------------------------+

sql/mdl.cc
@@ -4759,6 +4759,27 @@ bool MDL_context::has_locks(MDL_key::enum_mdl_namespace mdl_namespace) const {
 }
 
 /**
+  Does this context have locks other than the given namespace?
+
+  @retval true    At least one lock other than given namespace is held.
+  @retval false   No locks outside of the given namespace are held.
+*/
+bool MDL_context::has_locks_except(
+    MDL_key::enum_mdl_namespace mdl_namespace) const {
+  MDL_ticket *ticket;
+
+  for (int i = 0; i < MDL_DURATION_END; i++) {
+    enum_mdl_duration duration = static_cast<enum_mdl_duration>(i);
+
+    MDL_ticket_store::List_iterator it = m_ticket_store.list_iterator(duration);
+    while ((ticket = it++)) {
+      if (ticket->get_key()->mdl_namespace() != mdl_namespace) return true;
+    }
+  }
+  return false;
+}
+
+/**
   Do we hold any locks which are possibly being waited
   for by another MDL_context?
 
@@ -5183,12 +5204,30 @@ void MDL_mutex::unlock(THD *thd, MDL_ticket *ticket) {
 /**
   Guard constructor locks the mutex.
 */
-MDL_mutex_guard::MDL_mutex_guard(MDL_mutex *mdl_mutex, THD *thd)
-    : m_mdl_mutex(mdl_mutex), m_thd(thd) {
-  m_ticket = m_mdl_mutex->lock(m_thd);
+MDL_mutex_guard::MDL_mutex_guard(MDL_mutex *mdl_mutex, THD *thd,
+                                 mysql_mutex_t *mutex)
+    : m_mdl_mutex(mdl_mutex), m_thd(thd), m_mutex(mutex) {
+  assert(m_mdl_mutex);
+  assert(m_thd);
+
+  // If regular mutex is not passed in then ignore use_mdl_mutex flag.
+  if (use_mdl_mutex || !m_mutex) {
+    m_ticket = m_mdl_mutex->lock(m_thd);
+  } else {
+    mysql_mutex_lock(m_mutex);
+  }
 }
 
 /**
   Guard destructor unlocks the mutex.
 */
-MDL_mutex_guard::~MDL_mutex_guard() { m_mdl_mutex->unlock(m_thd, m_ticket); }
+MDL_mutex_guard::~MDL_mutex_guard() {
+  // Don't look at use_mdl_mutex any more and unlock whatever mutex was locked.
+  if (m_ticket) {
+    assert(m_mdl_mutex);
+    m_mdl_mutex->unlock(m_thd, m_ticket);
+  } else {
+    assert(m_mutex);
+    mysql_mutex_unlock(m_mutex);
+  }
+}

sql/mysqld_thd_manager.cc
@@ -46,7 +46,6 @@
 #include "my_sys.h"
 #include "mysql/components/services/bits/psi_bits.h"
 #include "mysql/thread_pool_priv.h"  // inc_thread_created
-#include "sql/mysqld.h"              // mdl_mutex_thread_remove
 #include "sql/rpl_source.h"          // unregister_replica
 #include "sql/sql_class.h"           // THD
 #include "thr_mutex.h"
@@ -253,39 +252,24 @@ void Global_THD_manager::remove_thd(THD *thd) {
   assert(cur_thd);
 
   const int partition = thd_partition(thd->thread_id());
-  MDL_ticket *ticket = nullptr;
+  MDL_mutex_guard guard(&mdl_mutex_thd_remove[partition], cur_thd,
+                        &LOCK_thd_remove[partition]);
+  MUTEX_LOCK(lock_list, &LOCK_thd_list[partition]);
 
-  if (mdl_mutex_thread_remove) {
-    ticket = mdl_mutex_thd_remove[partition].lock(cur_thd);
-  } else {
-    mysql_mutex_lock(&LOCK_thd_remove[partition]);
-  }
+  assert(unit_test || thd->release_resources_done());
 
-  // Scope for LOCK_thd_list mutex.
-  {
-    MUTEX_LOCK(lock_list, &LOCK_thd_list[partition]);
+  /*
+    Used by binlog_reset_master.  It would be cleaner to use
+    DEBUG_SYNC here, but that's not possible because the THD's debug
+    sync feature has been shut down at this point.
+  */
+  DBUG_EXECUTE_IF("sleep_after_lock_thread_count_before_delete_thd", sleep(5););
 
-    assert(unit_test || thd->release_resources_done());
-
-    /*
-      Used by binlog_reset_master.  It would be cleaner to use
-      DEBUG_SYNC here, but that's not possible because the THD's debug
-      sync feature has been shut down at this point.
-    */
-    DBUG_EXECUTE_IF("sleep_after_lock_thread_count_before_delete_thd", sleep(5););
-
-    const size_t num_erased = thd_list[partition].erase_unique(thd);
-    if (num_erased == 1) --atomic_global_thd_count;
-    // Removing a THD that was never added is an error.
-    assert(1 == num_erased);
-    mysql_cond_broadcast(&COND_thd_list[partition]);
-  }
-
-  if (mdl_mutex_thread_remove) {
-    mdl_mutex_thd_remove[partition].unlock(cur_thd, ticket);
-  } else {
-    mysql_mutex_unlock(&LOCK_thd_remove[partition]);
-  }
+  const size_t num_erased = thd_list[partition].erase_unique(thd);
+  if (num_erased == 1) --atomic_global_thd_count;
+  // Removing a THD that was never added is an error.
+  assert(1 == num_erased);
+  mysql_cond_broadcast(&COND_thd_list[partition]);
 }
 
 my_thread_id Global_THD_manager::get_new_thread_id() {
@@ -326,14 +310,10 @@ void Global_THD_manager::do_for_all_thd_copy(Do_THD_Impl *func) {
   Do_THD doit(func);
   THD *cur_thd = current_thd;
   assert(cur_thd);
-  MDL_ticket *ticket = nullptr;
 
   for (int i = 0; i < NUM_PARTITIONS; i++) {
-    if (mdl_mutex_thread_remove) {
-      ticket = mdl_mutex_thd_remove[i].lock(cur_thd);
-    } else {
-      mysql_mutex_lock(&LOCK_thd_remove[i]);
-    }
+    MDL_mutex_guard guard(&mdl_mutex_thd_remove[i], cur_thd,
+                          &LOCK_thd_remove[i]);
 
     mysql_mutex_lock(&LOCK_thd_list[i]);
 
@@ -350,12 +330,6 @@ void Global_THD_manager::do_for_all_thd_copy(Do_THD_Impl *func) {
     std::for_each(thd_list_copy.begin(), thd_list_copy.end(), doit);
 
     DEBUG_SYNC_C("inside_do_for_all_thd_copy");
-
-    if (mdl_mutex_thread_remove) {
-      mdl_mutex_thd_remove[i].unlock(cur_thd, ticket);
-    } else {
-      mysql_mutex_unlock(&LOCK_thd_remove[i]);
-    }
   }
 }
 

mysql-test/r/mysqld--help-notwin.result
@@ -1047,11 +1047,6 @@ The following options may be given as the first argument:
  is only used when setting a lower/minimum bound on HLC
  using minimum_hlc_ns system variable. The default value
  is 300 secs
- --mdl-mutex-thread-remove 
- Use thread pool friendly MDL lock when removing
- connection threads. If disabled, the regular mutex is
- used.
- (Defaults to on; use --skip-mdl-mutex-thread-remove to disable.)
  --memlock           Lock mysqld in memory.
  --min-examined-row-limit=# 
  Don't write queries to slow log that examine fewer rows
@@ -2957,6 +2952,10 @@ The following options may be given as the first argument:
  --use-fb-json-functions=name 
  Use legacy behavior of FB json functions implemented in
  5.6
+ --use-mdl-mutex     Use thread pool friendly MDL lock instead of regular
+ mutex where instrumented. If disabled, the regular mutex
+ is used.
+ (Defaults to on; use --skip-use-mdl-mutex to disable.)
  -u, --user=name     Run mysqld daemon as user.
  --validate-config   Validate the server configuration specified by the user.
  --validate-schema-from-attributes 
@@ -3343,7 +3342,6 @@ max-user-connections 0
 max-waiting-queries 0
 max-write-lock-count 18446744073709551615
 maximum-hlc-drift-ns 300000000000
-mdl-mutex-thread-remove TRUE
 memlock FALSE
 min-examined-row-limit 0
 min-examined-row-limit-sql-stats 0
@@ -3886,6 +3884,7 @@ updatable-views-with-limit YES
 update-binlog-pos-threshold 5242880
 upgrade AUTO
 use-fb-json-functions 
+use-mdl-mutex TRUE
 validate-config FALSE
 validate-schema-from-attributes FALSE
 validate-user-plugins TRUE

mysql-test/suite/sys_vars/r/mdl_mutex_thread_remove_basic.result
@@ -1,29 +1,0 @@
-Default value of mdl_mutex_thread_remove is true
-SELECT @@global.mdl_mutex_thread_remove;
-@@global.mdl_mutex_thread_remove
-1
-SELECT @@session.mdl_mutex_thread_remove;
-ERROR HY000: Variable 'mdl_mutex_thread_remove' is a GLOBAL variable
-Expected error 'Variable is a GLOBAL variable'
-mdl_mutex_thread_remove is a read-only variable
-SET @@global.mdl_mutex_thread_remove = false;
-ERROR HY000: Variable 'mdl_mutex_thread_remove' is a read only variable
-SELECT @@global.mdl_mutex_thread_remove;
-@@global.mdl_mutex_thread_remove
-1
-Restart the server with non default value (OFF)
-# restart: --mdl_mutex_thread_remove=OFF
-SELECT @@global.mdl_mutex_thread_remove;
-@@global.mdl_mutex_thread_remove
-0
-mdl_mutex_thread_remove is still a read-only variable
-SET @@global.mdl_mutex_thread_remove = true;
-ERROR HY000: Variable 'mdl_mutex_thread_remove' is a read only variable
-SELECT @@global.mdl_mutex_thread_remove;
-@@global.mdl_mutex_thread_remove
-0
-Restart the server with default value (ON)
-# restart: 
-SELECT @@global.mdl_mutex_thread_remove;
-@@global.mdl_mutex_thread_remove
-1

mysql-test/suite/sys_vars/r/use_mdl_mutex_basic.result
@@ -1,0 +1,29 @@
+Default value of use_mdl_mutex is true
+SELECT @@global.use_mdl_mutex;
+@@global.use_mdl_mutex
+1
+SELECT @@session.use_mdl_mutex;
+ERROR HY000: Variable 'use_mdl_mutex' is a GLOBAL variable
+Expected error 'Variable is a GLOBAL variable'
+use_mdl_mutex is a read-only variable
+SET @@global.use_mdl_mutex = false;
+ERROR HY000: Variable 'use_mdl_mutex' is a read only variable
+SELECT @@global.use_mdl_mutex;
+@@global.use_mdl_mutex
+1
+Restart the server with non default value (OFF)
+# restart: --use_mdl_mutex=OFF
+SELECT @@global.use_mdl_mutex;
+@@global.use_mdl_mutex
+0
+use_mdl_mutex is still a read-only variable
+SET @@global.use_mdl_mutex = true;
+ERROR HY000: Variable 'use_mdl_mutex' is a read only variable
+SELECT @@global.use_mdl_mutex;
+@@global.use_mdl_mutex
+0
+Restart the server with default value (ON)
+# restart: 
+SELECT @@global.use_mdl_mutex;
+@@global.use_mdl_mutex
+1

mysql-test/suite/sys_vars/t/use_mdl_mutex_basic.test
@@ -3,38 +3,38 @@
 ####
 # Verify default value true
 ####
---echo Default value of mdl_mutex_thread_remove is true
-SELECT @@global.mdl_mutex_thread_remove;
+--echo Default value of use_mdl_mutex is true
+SELECT @@global.use_mdl_mutex;
 
 ####
 # Verify that this is not a session variable
 ####
 --Error ER_INCORRECT_GLOBAL_LOCAL_VAR
-SELECT @@session.mdl_mutex_thread_remove;
+SELECT @@session.use_mdl_mutex;
 --echo Expected error 'Variable is a GLOBAL variable'
 
---echo mdl_mutex_thread_remove is a read-only variable
+--echo use_mdl_mutex is a read-only variable
 --Error ER_INCORRECT_GLOBAL_LOCAL_VAR
-SET @@global.mdl_mutex_thread_remove = false;
-SELECT @@global.mdl_mutex_thread_remove;
+SET @@global.use_mdl_mutex = false;
+SELECT @@global.use_mdl_mutex;
 
 ####
 ## Restart the server with a non default value of the variable
 ####
 --echo Restart the server with non default value (OFF)
---let $restart_parameters="restart: --mdl_mutex_thread_remove=OFF"
+--let $restart_parameters="restart: --use_mdl_mutex=OFF"
 --source include/restart_mysqld.inc
 
-SELECT @@global.mdl_mutex_thread_remove;
+SELECT @@global.use_mdl_mutex;
 
---echo mdl_mutex_thread_remove is still a read-only variable
+--echo use_mdl_mutex is still a read-only variable
 --Error ER_INCORRECT_GLOBAL_LOCAL_VAR
-SET @@global.mdl_mutex_thread_remove = true;
-SELECT @@global.mdl_mutex_thread_remove;
+SET @@global.use_mdl_mutex = true;
+SELECT @@global.use_mdl_mutex;
 
 
 --echo Restart the server with default value (ON)
 --let $restart_parameters="restart: "
 --source include/restart_mysqld.inc
 
-SELECT @@global.mdl_mutex_thread_remove;
+SELECT @@global.use_mdl_mutex;

mysql-test/suite/x/t/session_close.test
@@ -51,15 +51,15 @@ send SELECT user from information_schema.processlist order by user;
 --echo # I.3
 # Go back to C0, wait for X1
 --connection default
---let $mdl_mutex_thread_remove = `select @@global.mdl_mutex_thread_remove`
-if ($mdl_mutex_thread_remove) {
+--let $use_mdl_mutex = `select @@global.use_mdl_mutex`
+if ($use_mdl_mutex) {
   let $wait_condition=
     select count(*) from performance_schema.events_waits_current
         where EVENT_NAME = 'wait/lock/metadata/sql/mdl' and 
               OBJECT_TYPE = 'MUTEX' and
               OBJECT_NAME like 'THREAD_REMOVE%';
 }
-if (!$mdl_mutex_thread_remove) {
+if (!$use_mdl_mutex) {
   let $wait_condition=
     select count(*) from performance_schema.events_waits_current
         where EVENT_NAME like 'wait/synch/mutex/sql/LOCK_thd_remove';

mysql-test/suite/x/t/session_close_mutex-master.opt
@@ -1,1 +1,1 @@
---mdl_mutex_thread_remove=OFF
+--use_mdl_mutex=OFF

commit 810e8ea5a2cebd90be81753d76f3db9a6667c5b2

Fix ThreadManagerTest.AddRemoveTHDWithGuard
It fails after changes introduced in https://github.com/facebook/mysql-5.6/commit/d630c52b336

Squash with D45162083

sql/mdl.cc
@@ -5209,10 +5209,10 @@ MDL_mutex_guard::MDL_mutex_guard(MDL_mutex *mdl_mutex, THD *thd,
                                  mysql_mutex_t *mutex)
     : m_mdl_mutex(mdl_mutex), m_thd(thd), m_mutex(mutex) {
   assert(m_mdl_mutex);
-  assert(m_thd);
 
   // If regular mutex is not passed in then ignore use_mdl_mutex flag.
   if (use_mdl_mutex || !m_mutex) {
+    assert(m_thd);
     m_ticket = m_mdl_mutex->lock(m_thd);
   } else {
     mysql_mutex_lock(m_mutex);

sql/mysqld_thd_manager.cc
@@ -46,6 +46,7 @@
 #include "my_sys.h"
 #include "mysql/components/services/bits/psi_bits.h"
 #include "mysql/thread_pool_priv.h"  // inc_thread_created
+#include "sql/mysqld.h"              // use_mdl_mutex
 #include "sql/rpl_source.h"          // unregister_replica
 #include "sql/sql_class.h"           // THD
 #include "thr_mutex.h"
@@ -309,7 +310,7 @@ void Global_THD_manager::wait_till_no_thd() {
 void Global_THD_manager::do_for_all_thd_copy(Do_THD_Impl *func) {
   Do_THD doit(func);
   THD *cur_thd = current_thd;
-  assert(cur_thd);
+  if (use_mdl_mutex) assert(cur_thd);
 
   for (int i = 0; i < NUM_PARTITIONS; i++) {
     MDL_mutex_guard guard(&mdl_mutex_thd_remove[i], cur_thd,

unittest/gunit/thd_manager-t.cc
@@ -40,6 +40,8 @@
 using thread::Notification;
 using thread::Thread;
 
+extern bool use_mdl_mutex;
+
 namespace thd_manager_unittest {
 
 class ThreadManagerTest : public ::testing::Test {
@@ -67,6 +69,8 @@ enum TEST_TYPE { TEST_WAIT = 0, TEST_TIMED_WAIT = 1 };
   Verify add_thd(), remove_thd() methods
 */
 TEST_F(ThreadManagerTest, AddRemoveTHDWithGuard) {
+  use_mdl_mutex = false;
+
   THD thd1(false), thd2(false);
   thd1.server_id = 1;
   thd1.set_new_thread_id();